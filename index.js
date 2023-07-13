f6272db4f193ed464dd8d799cfc310f92c12d673
tobias <tobias.omondi@student.moringaschool.com>
2023-07-13 20:30:13 +0300

    my-work

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun();
+
+function mondayWork(toDo = 'go to the office') {
+    return `This Monday, I will ${toDo}.`
+}
+
+mondayWork();
+
+function wrapAdjective(symbol = "*") {
+    return function(adjective = "special") {
+        return `You are ${symbol}${adjective}${symbol}!`
+    }
+}
+
+wrapAdjective()();
